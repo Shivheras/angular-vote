3dce567e10b37300167bae656b4ebb078d1d0145
Shivheras <shivheras@gmail.com>
2020-02-23 14:52:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-redi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-redi)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ArticleComponent } from './article/article.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule],
+  declarations: [ AppComponent, HelloComponent, ArticleComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
